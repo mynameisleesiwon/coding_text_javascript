b46e06660a8f405c3a81417fb0dd8f29eaae2647
mynameisleesiwon <seoil1221@gmail.com>
2024-02-15 11:32:41 +0900

    day7

---
+++ 코딩테스트 입문/Day7.js
@@ -0,0 +1,67 @@
+// 1. 특정 문자 제거하기
+
+// 문제 설명
+// 문자열 my_string과 문자 letter이 매개변수로 주어집니다. my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ my_string의 길이 ≤ 100
+// letter은 길이가 1인 영문자입니다.
+// my_string과 letter은 알파벳 대소문자로 이루어져 있습니다.
+// 대문자와 소문자를 구분합니다.
+
+function test1(my_string, letter) {
+  return my_string
+    .split("")
+    .filter((item) => item != letter)
+    .join("");
+}
+
+// 2. 각도기
+
+// 문제 설명
+// 각에서 0도 초과 90도 미만은 예각, 90도는 직각, 90도 초과 180도 미만은 둔각 180도는 평각으로 분류합니다.
+// 각 angle이 매개변수로 주어질 때 예각일 때 1, 직각일 때 2, 둔각일 때 3, 평각일 때 4를 return하도록 solution 함수를 완성해주세요.
+
+// 예각 : 0 < angle < 90
+// 직각 : angle = 90
+// 둔각 : 90 < angle < 180
+// 평각 : angle = 180
+
+function test2(angle) {
+  if (angle === 180) return 4;
+  if (angle > 90) return 3;
+  if (angle === 90) return 2;
+  return 1;
+}
+
+// 3. 양꼬치
+
+// 문제 설명
+// 머쓱이네 양꼬치 가게는 10인분을 먹으면 음료수 하나를 서비스로 줍니다.
+// 양꼬치는 1인분에 12,000원, 음료수는 2,000원입니다. 정수 n과 k가 매개변수로 주어졌을 때,
+// 양꼬치 n인분과 음료수 k개를 먹었다면 총얼마를 지불해야 하는지 return 하도록 solution 함수를 완성해보세요.
+
+// 제한사항
+// 0 < n < 1,000
+// n / 10 ≤ k < 1,000
+// 서비스로 받은 음료수는 모두 마십니다.
+
+function test3(n, k) {
+  return n * 12000 + (k - Math.floor(n / 10)) * 2000;
+}
+
+// 4. 짝수의 합
+
+// 문제 설명
+// 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+
+// 제한사항
+// 0 < n ≤ 1000
+
+function test4(n) {
+  let answer = 0;
+  for (let i = 1; i <= Math.floor(n / 2); i++) {
+    answer += i * 2;
+  }
+  return answer;
+}
