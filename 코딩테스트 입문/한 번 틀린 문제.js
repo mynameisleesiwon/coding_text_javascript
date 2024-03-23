a586c291002fdc3993b9d356ba3784fd8891709e
mynameisleesiwon <seoil1221@gmail.com>
2024-03-23 13:35:15 +0900

    한 번 틀린 문제 : 중앙값 구하기

--- 코딩테스트 입문/한 번 틀린 문제.js
+++ 코딩테스트 입문/한 번 틀린 문제.js
@@ -19,13 +19,34 @@ function gcd(a, b) {
 }
 
 function test1(numer1, denom1, numer2, denom2) {
+  // 분수의 덧셈
   let sumNumer = numer1 * denom2 + numer2 * denom1;
   let sumDenom = denom1 * denom2;
 
-  let gcbValue = gcb(sumNumer, sumDenom);
+  // 최대공약수 계산
+  const gcdValue = gcd(sumNumer, sumDenom);
 
-  sumNumer /= gcbValue;
-  sumDenom /= gcbValue;
+  // 기약 분수로 만들기
+  sumNumer /= gcdValue;
+  sumDenom /= gcdValue;
 
+  // 결과 배열 반환
   return [sumNumer, sumDenom];
+}
+
+// 2. 중앙값 구하기 ⭕
+
+// 문제 설명
+// 중앙값은 어떤 주어진 값들을 크기의 순서대로 정렬했을 때 가장 중앙에 위치하는 값을 의미합니다.
+// 예를 들어 1, 2, 7, 10, 11의 중앙값은 7입니다.
+// 정수 배열 array가 매개변수로 주어질 때, 중앙값을 return 하도록 solution 함수를 완성해보세요.
+
+// 제한사항
+// array의 길이는 홀수입니다.
+// 0 < array의 길이 < 100
+// -1,000 < array의 원소 < 1,000
+
+function test2(array) {
+  let middleNum = Math.floor(array.length / 2);
+  return array.sort((a, b) => a - b)[middleNum];
 }
