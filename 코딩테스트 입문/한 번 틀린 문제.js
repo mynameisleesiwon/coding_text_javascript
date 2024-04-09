3c39b4aac4bea269f4b5e44e6a11889406de5cc3
mynameisleesiwon <seoil1221@gmail.com>
2024-04-09 10:56:51 +0900

    한 번 틀린 문제 :  구슬을 나누는 경우의 수

--- 코딩테스트 입문/한 번 틀린 문제.js
+++ 코딩테스트 입문/한 번 틀린 문제.js
@@ -293,3 +293,29 @@ function test10(hp) {
 
   return count;
 }
+
+// 11. 구슬을 나누는 경우의 수 ❌
+
+// 문제 설명
+// 머쓱이는 구슬을 친구들에게 나누어주려고 합니다. 구슬은 모두 다르게 생겼습니다.
+// 머쓱이가 갖고 있는 구슬의 개수 balls와 친구들에게 나누어 줄 구슬 개수 share이 매개변수로 주어질 때,
+// balls개의 구슬 중 share개의 구슬을 고르는 가능한 모든 경우의 수를 return 하는 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ balls ≤ 30
+// 1 ≤ share ≤ 30
+// 구슬을 고르는 순서는 고려하지 않습니다.
+// share ≤ balls
+
+function test11(balls, share) {
+  let result = 1; // 결과 값을 저장할 변수를 1로 초기화합니다.
+
+  // share 개수만큼 반복하면서 조합의 공식을 적용합니다.
+  for (let i = 0; i < share; i++) {
+    result *= balls - i; // 분자에 해당하는 balls! 부분을 계산합니다.
+    result /= i + 1; // 분모에 해당하는 k!(n-k)! 부분을 계산합니다.
+  }
+
+  // 모든 계산이 끝난 후, 가능한 모든 경우의 수를 반환합니다.
+  return result;
+}
