050592ad291a743fd01a48ab841941312f495e9e
mynameisleesiwon <seoil1221@gmail.com>
2024-04-11 12:05:30 +0900

    한 번 틀린 문제 :  공 던지기

--- 코딩테스트 입문/한 번 틀린 문제.js
+++ 코딩테스트 입문/한 번 틀린 문제.js
@@ -319,3 +319,40 @@ function test11(balls, share) {
   // 모든 계산이 끝난 후, 가능한 모든 경우의 수를 반환합니다.
   return result;
 }
+
+// 12. 공 던지기 ⭕
+
+// 문제 설명
+// 머쓱이는 친구들과 동그랗게 서서 공 던지기 게임을 하고 있습니다.
+// 공은 1번부터 던지며 오른쪽으로 한 명을 건너뛰고 그다음 사람에게만 던질 수 있습니다.
+// 친구들의 번호가 들어있는 정수 배열 numbers와 정수 K가 주어질 때,
+// k번째로 공을 던지는 사람의 번호는 무엇인지 return 하도록 solution 함수를 완성해보세요.
+
+// 제한사항
+// 2 < numbers의 길이 < 100
+// 0 < k < 1,000
+// numbers의 첫 번째와 마지막 번호는 실제로 바로 옆에 있습니다.
+// numbers는 1부터 시작하며 번호는 순서대로 올라갑니다.
+
+function test12(numbers, k) {
+  let count = k - 1; // k-1번 공을 던집니다. (첫 번째 던지기를 0으로 계산하기 때문에)
+  let index = 0; // 현재 공을 받는 사람의 인덱스
+  let result = 1; // 결과를 저장할 변수, 초기값은 1번 사람으로 설정
+
+  while (count > 0) {
+    index += 2; // 오른쪽으로 한 명을 건너뛰고 다음 사람에게 공을 던짐
+
+    if (numbers[index]) {
+      // 만약 index 위치에 사람(요소)이 있다면
+      result = numbers[index]; // 결과를 해당 사람의 번호로 업데이트
+    } else {
+      // index가 배열의 범위를 초과하는 경우
+      result = numbers[index - numbers.length]; // index를 배열 길이만큼 빼서 순환
+      index = index - numbers.length; // index 업데이트
+    }
+
+    count--; // 공을 던진 횟수를 감소
+  }
+
+  return result; // 결과 반환
+}
