249c204a9c30eeb6b31f7de4fb3066529d1649ef
mynameisleesiwon <seoil1221@gmail.com>
2024-04-18 10:16:58 +0900

    한 번 틀린 문제 :  영어가 싫어요

--- 코딩테스트 입문/한 번 틀린 문제.js
+++ 코딩테스트 입문/한 번 틀린 문제.js
@@ -481,7 +481,7 @@ function isUpperCase(string) {
   return string.toUpperCase() === string;
 }
 
-function solution(my_string) {
+function test16(my_string) {
   // 문자열을 배열로 변환합니다.
   let characters = my_string.split("");
 
@@ -492,4 +492,52 @@ function solution(my_string) {
 
   // 변환된 문자 배열을 다시 문자열로 합칩니다.
   return changedCharacters.join("");
+}
+
+// 17. 영어가 싫어요 ⭕
+
+// 문제 설명
+// 영어가 싫은 머쓱이는 영어로 표기되어있는 숫자를 수로 바꾸려고 합니다.
+// 문자열 numbers가 매개변수로 주어질 때, numbers를 정수로 바꿔 return 하도록 solution 함수를 완성해 주세요.
+
+// 제한사항
+// numbers는 소문자로만 구성되어 있습니다.
+// numbers는 "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" 들이 공백 없이 조합되어 있습니다.
+// 1 ≤ numbers의 길이 ≤ 50
+// "zero"는 numbers의 맨 앞에 올 수 없습니다.
+
+function test17(numbers) {
+  // 알파벳으로 표기된 숫자와 실제 숫자를 매핑하기 위한 객체
+  let alphabetToNumObject = {
+    zero: "0",
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9",
+  };
+
+  // 알파벳 조합을 저장할 변수
+  let alphabetCombination = "";
+  // 최종적으로 변환된 숫자를 저장할 변수
+  let alphabetToNumber = "";
+
+  // 주어진 문자열을 하나씩 순회하면서 처리
+  numbers.split("").forEach((item) => {
+    // 현재까지의 알파벳 조합에 현재 알파벳을 추가
+    alphabetCombination += item;
+    // 만약 현재의 알파벳 조합이 숫자로 변환될 수 있다면
+    if (alphabetToNumObject[alphabetCombination]) {
+      // 변환된 숫자를 결과 문자열에 추가하고 알파벳 조합을 초기화
+      alphabetToNumber += alphabetToNumObject[alphabetCombination];
+      alphabetCombination = "";
+    }
+  });
+
+  // 최종적으로 변환된 숫자 문자열을 정수로 변환하여 반환
+  return parseInt(alphabetToNumber);
 }
